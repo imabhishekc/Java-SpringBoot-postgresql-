7dd7d9140faead90aa076d362d1f0478fda10b18
Abhishek Chakraborty <85833164+imabhishekc@users.noreply.github.com>
2023-05-22 13:22:04 +0545

    Add files via upload

---
+++ ProductSpringProject/src/main/java/com/abhishek/productspringproject/ProductSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.productspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductSpringProjectApplication.class, args);
+    }
+
+}
---
+++ ProductSpringProject/src/test/java/com/abhishek/productspringproject/ProductSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.productspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
